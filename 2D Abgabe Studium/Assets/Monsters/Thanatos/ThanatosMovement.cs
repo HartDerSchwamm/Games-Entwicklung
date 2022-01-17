3ddae4e626222019f38689245eeb5cb11a530b31
Elonex13 <58557026+Elonex13@users.noreply.github.com>
2022-01-17 08:31:09 +0100

    added thanathos flip and skelleton attack fix

--- 2D Abgabe Studium/Assets/Monsters/Thanatos/ThanatosMovement.cs
+++ 2D Abgabe Studium/Assets/Monsters/Thanatos/ThanatosMovement.cs
@@ -17,8 +17,16 @@ public class ThanatosMovement : MonoBehaviour
     {
         Vector3 playerposition = player.transform.position;
         Vector3 enemyPosition = gameObject.transform.position;
-        float distance = Mathf.Abs(Vector3.Distance(playerposition, enemyPosition));
-        if (distance < attackRange)
+        float distance = Vector3.Distance(playerposition, enemyPosition);
+        float absDistance = Mathf.Abs(distance);
+        if (player.transform.position.x < gameObject.transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        } else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (absDistance < attackRange)
         {
             anim.SetBool("attack",true);
         }else
