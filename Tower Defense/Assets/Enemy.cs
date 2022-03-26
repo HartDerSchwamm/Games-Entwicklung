5727363abfe3d2c95bbb2f8108ee1d6ce2d1a436
Benny <58556882+HartDerSchwamm@users.noreply.github.com>
2022-03-26 22:14:41 +0100

    Added Map, Enemy, Waypoints, Scripts for Enemy

---
+++ Tower Defense/Assets/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = WayPoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    // Go trought the Array Waypoints to get the next Waypoint.
+    void GetNextWaypoint()
+    {
+        if(wavePointIndex >= WayPoints.points.Length -1 )
+        {
+            Destroy(gameObject);
+        }
+        else {
+            wavePointIndex++;
+            target = WayPoints.points[wavePointIndex];
+        }
+      
+    }
+}
